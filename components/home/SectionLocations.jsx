be1806021952c523a5a2759482198ecc623ce25c
react <yangsook.kim@gmail.com>
2022-06-07 10:51:27 -0700

    modified main page

--- components/home/SectionLocations.jsx
+++ components/home/SectionLocations.jsx
@@ -19,109 +19,89 @@ const SectionLocations = ({ isMobile }) => {
           align="center"
           sx={{ mb: 4 }}
         >
-          Where We are Rollin’
+          Where we are Rollin’
         </Typography>
 
-        <Grid container sx={{ mb: 1 }} spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <Image
-              src="/home_page/location-garland.jpg"
-              alt="Location Garland"
-              width="592"
-              height="420"
-            />
-            <Grid item display="row">
-              {/* <Image
-                src={"/icons/logo-circle.svg"}
-                width={isMobile ? "18" : "18"}
-                height={isMobile ? "18" : "18"}
-              /> */}
+        <Grid container sx={{ mb: 1 }} spacing={2} align="center">
 
-              <Typography
-                color="text"
-                variant={isMobile ? "h4" : "h5"}
-                align="left"
-                sx={isMobile ? { mb: 2, mt: 2 } : { mb: 2, mt: 4 }}
+        <Grid container item xs={12} sm={6} justifyContent="center" direction="col">
+            <Grid item>
+              <Image
+                src="/home_page/location-garland.jpg"
+                alt="Location Garland"
+                width="592"
+                height="420"
+              />
+            </Grid>
+            <Grid container item gap={1} sx={isMobile ? { mb: 2, mt: 2 } : { mb: 2, mt: 4 }} direction="row" justifyContent="center">
+              <Grid item>
+                  <Image src={"/icons/logo-circle.svg"} width={18} height={18} />
+              </Grid>
+              <Grid item alignSelf='left'>
+                  <Typography color="text" variant={isMobile ? "h4" : "h5"}>
+                    Sushi Rollin' Garland
+                  </Typography>
+              </Grid>
+            </Grid>
+            <Grid item >
+              <Typography color="text" variant={isMobile ? "body2" : "body1"}>
+              <MuiNextLink
+                href="https://goo.gl/maps/JaVAgjRh7ZKrgMNC8"
+                target="_blank"
               >
-                Sushi Rollin' Garland
+              6850 N Shiloh Rd, Garland, TX 75044
+              </MuiNextLink>
+
+              </Typography>
+              <Typography color="text" variant={isMobile ? "body2" : "body1"} sx={{ mb: 2 }}>
+                Opens Daily 11 a.m. - 9:30 p.m.
+              </Typography>
+              <Typography color="text" variant={isMobile ? "body2" : "body1"} sx={{ mb: 1 }}>
+                (469) 798-7831
               </Typography>
             </Grid>
-            <Typography
-              color="text"
-              align="left"
-              variant={isMobile ? "body2" : "body1"}
-            >
-              6850 N Shiloh Rd, Garland, TX 75044
-            </Typography>
-            <Typography
-              color="text"
-              variant={isMobile ? "body2" : "body1"}
-              align="left"
-              fontFamily={"Nunito"}
-              sx={{ mb: 2 }}
-            >
-              Opens Daily 11 a.m. - 9:30 p.m.
-            </Typography>
-            <Typography
-              color="text"
-              variant={isMobile ? "body2" : "body1"}
-              align="left"
-              fontFamily={"Nunito"}
-              sx={{ mb: 1 }}
-            >
-              (469) 798-7831
-            </Typography>
           </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Image
-              //
-              src="/home_page/location-lewisville.jpg"
-              alt="Location Lewisville"
-              width="592"
-              height="420"
-            />
-
-            {/* <Image
-              src={"/icons/logo-circle.svg"}
-              width={isMobile ? "18" : "18"}
-              height={isMobile ? "18" : "18"}
-            /> */}
-
-            <Typography
-              color="text"
-              variant={isMobile ? "h4" : "h5"}
-              align="left"
-              sx={isMobile ? { mb: 2, mt: 2 } : { mb: 2, mt: 4 }}
-            >
-              Sushi Rollin' Lewisville
-            </Typography>
-            <Typography
-              color="text"
-              variant={isMobile ? "body2" : "body1"}
-              align="left"
-            >
+          <Grid container item xs={12} sm={6} justifyContent="center" direction="col">
+            <Grid item>
+              <Image
+                src="/home_page/location-lewisville.jpg"
+                alt="Location Garland"
+                width="592"
+                height="420"
+              />
+            </Grid>
+            <Grid container item gap={1} sx={isMobile ? { mb: 2, mt: 2 } : { mb: 2, mt: 4 }} direction="row" justifyContent="center">
+              <Grid item>
+                  <Image src={"/icons/logo-circle.svg"} width={18} height={18} />
+              </Grid>
+              <Grid item alignSelf='left'>
+                  <Typography color="text" variant={isMobile ? "h4" : "h5"}>
+                    Sushi Rollin' Lewisville
+                  </Typography>
+              </Grid>
+            </Grid>
+            <Grid item >
+              <Typography color="text" variant={isMobile ? "body2" : "body1"}>
+              <MuiNextLink
+                href="https://goo.gl/maps/7s3axUUgB9FmVUfT8"
+                target="_blank"
+              >
               1288 W Main St #142, Lewisville, TX 75067
-            </Typography>
-            <Typography
-              color="text"
-              variant={isMobile ? "body2" : "body1"}
-              align="left"
-              sx={{ mb: 2 }}
-            >
-              Opens Daily 11 a.m. - 9:00 p.m.
-            </Typography>
-            <Typography
-              color="text"
-              variant={isMobile ? "body2" : "body1"}
-              align="left"
-            >
-              (817) 320-3722
-            </Typography>
+              </MuiNextLink>
+              </Typography>
+              <Typography color="text" variant={isMobile ? "body2" : "body1"} sx={{ mb: 2 }}>
+                Opens Daily 11 a.m. - 9:00 p.m.
+              </Typography>
+              <Typography color="text" variant={isMobile ? "body2" : "body1"} sx={{ mb: 1 }}>
+                (817) 320-3722
+              </Typography>
+            </Grid>
           </Grid>
+
+
         </Grid>
 
-        <Grid align="center" pt={4} pb={6}>
+        <Grid align="center" pt={4} pb={2}>
           <MuiNextLink href="/locations" underline="none" sx={{ py: 2 }}>
             <Button>View More Store Pictures</Button>
           </MuiNextLink>
